a788a6d82a07a335ed5c92ee9e29b1c0fdac322c
LiParanoid <lipengyx163@163.com>
2018-12-09 20:08:37 +0800

    添加jackson 依赖;添加通用返回信息类,返回json;修改对应controller

--- src/main/java/com/rytong/crud/controller/EmployeeController.java
+++ src/main/java/com/rytong/crud/controller/EmployeeController.java
@@ -3,12 +3,15 @@ package com.rytong.crud.controller;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.rytong.crud.bean.Employee;
+import com.rytong.crud.bean.Msg;
 import com.rytong.crud.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -22,11 +25,32 @@ import java.util.List;
 public class EmployeeController {
     @Autowired
     EmployeeService employeeService;
+
+    /**
+     * 需要导入Jackson包
+     * @param pn
+     * @return
+     */
+    @RequestMapping("/emps")
+    @ResponseBody
+    public Msg getEmpsWithJson(@RequestParam(value = "pn",defaultValue = "1")Integer pn){
+        // 会查出所有 不是一个分页查询
+        //引入PageHelper插件
+        // 在查询之前只需要调用
+        PageHelper.startPage(pn, 5);
+        // startPage后面紧跟的这个查询就是一个分页查询
+        List<Employee> emps = employeeService.getAll();
+        // 使用pageInfo包装查询后的结果，只需要将pageInfo交给页面就行了。
+        // 封装了详细的分页信息,包括有我们查询出来的数据，传入连续显示的页数
+        PageInfo page = new PageInfo(emps, 5);
+
+        return Msg.success().add("pageInfo",page);
+    }
     /**
      * 查询员工数据
      * @return
      */
-    @RequestMapping("/emps")
+    //@RequestMapping("/emps")
     public String getEmps(@RequestParam(value = "pn",defaultValue = "1")Integer pn, Model model){
         // 会查出所有 不是一个分页查询
         //引入PageHelper插件
